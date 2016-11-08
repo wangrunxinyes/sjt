0cb3f7661462c2a4e3e3c3f6852ab77c81014f11
wangrunxin <wangrunxinyes@gmail.com>
2016-11-08 17:49:42 +0800

    -update;

--- frontend/views/system/about.php
+++ frontend/views/system/about.php
@@ -14,11 +14,11 @@ AboutAsset::register($this);
             <div class="post-content">
                 <p>上海交通大学网成立于 2015 年 11 月 09日, 现在正在试运行阶段，欢迎大家提出宝贵意见。</p>
                 <hr>
-                <p>管理员
+                <p style="display: none;">管理员
                 <br>
                     <strong>李京波</strong></p>
                 <p>
-                 邮件地址
+                 管理员邮件地址
                     <br>
                     <strong><a href=mailto:739023880@qq.com>739023880@qq.com</a></strong>
                 </p>
